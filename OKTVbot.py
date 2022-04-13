7ddd336c092b6517a6a00a241cc24e7bf794ca45
OkbiQuant <95954787+OkbiQuant@users.noreply.github.com>
2022-04-13 20:41:47 +0800

    Update OKTVbot.py

--- OKTVbot.py
+++ OKTVbot.py
@@ -11,9 +11,6 @@ import os
 app = Flask(__name__)
 
 
-# order_side = ''
-
-
 @app.route('/')
 def hello_world():
     return 'hello world'
@@ -31,131 +28,109 @@ def trader():
     json_data = json.loads(data_last_line)
     print(json_data)
 
-    config = {}
-    if os.path.exists('./config.json'):
-        config = json.load(open('./config.json', encoding="UTF-8"))
+    simple_config = {}
+    if os.path.exists('./simple_config.json'):
+        simple_config = json.load(open('./simple_config.json', encoding="UTF-8"))
     else:
         logging.info("配置文件 config.json 不存在，程序即将退出")
         exit()
 
-    config_users = config['users']
-
+    exchange = json_data['exchange']
+    exchange_type = json_data['exchange_type']
     symbol = json_data['symbol']
     side = json_data['side']
     position_amount = ''
+    author = 'https://t.me/okbi_com'
+    amount = json_data['amount']
+
+    if author != json_data['author']:
+        print(str('您在TradingView发送的数据，为了尊重作者，请加上 https://t.me/okbi_com, 重起软件'))
+        exit()
 
     try:
-        type = json_data['type']
+        # 交易所API账户配置
+        # 暂时只支持binance交易所，等待更新升级
+        if exchange == 'binance':
+            ex = ccxt.binance(config={
+                'apiKey': simple_config['exchanges']['binance']['api_key'],
+                'secret': simple_config['exchanges']['binance']['api_secret'],
+                'enableRateLimit': True,
+            })
+        elif exchange == 'okx':
+            ex = ccxt.okex5(config={
+                'apiKey': simple_config['exchanges']['okx']['api_key'],
+                'secret': simple_config['exchanges']['okx']['api_secret'],
+                'enableRateLimit': True,
+                'password': simple_config['exchanges']['okx']['password']
+            })
+        elif exchange == 'ftx':
+            ex = ccxt.ftx(config={
+                'apiKey': simple_config['exchanges']['ftx']['api_key'],
+                'secret': simple_config['exchanges']['ftx']['api_secret'],
+                'enableRateLimit': True
+            })
+        else:
+            print('交易所有错，没有这个交易所')
+
+        if (side == 'buy') or (side == 'sell'):
+            #  根据信号做多单，还是空单
+            print('准备在：', exchange, '交易所，执行买入多单或者空单操作！')
+            k = 0
+            while k < 10:
+                create_order_info = {}
+                print('第 ', k, ' 次尝试买入交易！')
+                try:
+                    create_order_info = ex.create_order(symbol, 'MARKET', side, amount,
+                                                        params={'type': exchange_type})
+                    print(create_order_info)
+                    if create_order_info:
+                        print('已经成功下单，退出循环')
+                        break
+                    time.sleep(3)
+                except Exception as e:
+                    print(str(e))
+                k = k + 1
+                time.sleep(3)
+
+        #  平仓信号
+        if side == 'TP':
+            print('准备在：', exchange, '交易所，执行市价全平操作！')
+            j = 0
+            while j < 10:
+                print('第 ', j, ' 次尝试平仓！')
+                create_take_profit_order_info = {}
+                positionRisk = binance.fapiPrivateV2GetPositionRisk()
+                for i in positionRisk:
+                    if i['symbol'] == 'BTCUSDT':
+                        print(i['positionAmt'])
+                        position_amount = i['positionAmt']
+                        break
+                print(position_amount)
+                # position_amount = float(position_amount)
+                # print(position_amount)
+                if position_amount > float(0):
+                    position_side = 'sell'
+                elif position_amount < float(0):
+                    position_side = 'buy'
+                    position_amount = abs(position_amount)
+                else:
+                    print('空仓，没有平仓交易发生！')
+                    break
+                try:
+                    create_take_profit_order_info = binance.create_order(symbol, 'MARKET', position_side,
+                                                                         position_amount,
+                                                                         params={'type': exchange_type})
+                    print(create_take_profit_order_info)
+                    if create_take_profit_order_info:
+                        break
+                except Exception as e:
+                    print(str(e))
+                j = j + 1
     except Exception as e:
         print(str(e))
 
-    for key in config_users:
-        try:
-            api_key = config_users[key]['api_key']
-            api_secret = config_users[key]['api_secret']
-            exchange = config_users[key]['exchange']
-            password = config_users[key]['password']
-            bot_tv_safe_connect_key = config_users[key]['bot_tv_safe_connect_key']
-
-            if bot_tv_safe_connect_key != json_data['bot_tv_safe_connect_key']:
-                break
-            if json_data['author'] != 'https://t.me/okbi_com':
-                break
-            contract_type = {
-                'type': config_users[key]['contract_type']
-            }
-            # 交易所API账户配置
-            try:
-                if exchange == 'binance':
-                    ex = ccxt.binance({
-                        'apiKey': api_key,
-                        'secret': api_secret,
-                        'enableRateLimit': True
-                    })
-                elif exchange == 'okex':
-                    ex = ccxt.okex5(config={
-                        'enableRateLimit': True,
-                        'apiKey': api_key,
-                        'secret': api_secret,
-                        # okex requires this: https://github.com/ccxt/ccxt/wiki/Manual#authentication
-                        'password': password,
-                        'verbose': False,  # for debug output
-                    })
-                elif exchange == 'ftx':
-                    ex = ccxt.binance({
-                        'apiKey': api_key,
-                        'secret': api_secret,
-                        'enableRateLimit': True
-                    })
-                elif exchange == 'ftx':
-                    ex = ccxt.bitmex({
-                        'apiKey': api_key,
-                        'secret': api_secret,
-                        'enableRateLimit': True
-                    })
-            except Exception as e:
-                print(str(e))
-
-            # 观察自己合约账户的资金状况
-
-            ex_balance_margin = ex.fetch_balance(contract_type)
-            print(ex_balance_margin['USDT'])  # USDT这个资产的数量
-
-            if (side == 'buy') or (side == 'sell'):
-                #  根据信号做多单，还是空单
-                amount = config_users[key]['amount']
-                try:
-                    create_order_info = ex.create_order(symbol, type, side, amount, params=contract_type)
-                    print(create_order_info)
-                except Exception as e:
-                    time.sleep(1)
-                    print(str(e))
-                    try:
-                        create_order_info = ex.create_order(symbol, type, side, amount, params=contract_type)
-                        print(create_order_info)
-                    except Exception as e:
-                        print(str(e))
-                        try:
-                            create_order_info = ex.create_order(symbol, type, side, amount, params=contract_type)
-                            print(create_order_info)
-                        except Exception as e:
-                            print(str(e))
-
-            #  平仓信号
-            if side == 'TP':
-                j = 0
-                while j < 3:
-                    print('平仓 ', j)
-                    positionRisk = ex.fapiPrivateV2GetPositionRisk()
-                    for i in positionRisk:
-                        if i['symbol'] == 'BTCUSDT':
-                            print(i['positionAmt'])
-                            position_amount = i['positionAmt']
-                            continue
-                    print(position_amount)
-                    position_amount = float(position_amount)
-                    print(position_amount)
-                    if position_amount > float(0):
-                        position_side = 'sell'
-                    elif position_amount < float(0):
-                        position_side = 'buy'
-                        position_amount = abs(position_amount)
-                    else:
-                        print('空仓，没有平仓交易发生！')
-                    try:
-                        create_take_profit_order_info = ex.create_order(symbol, 'MARKET', position_side, position_amount,
-                                                                        params=contract_type)
-                        print(create_take_profit_order_info)
-                    except Exception as e:
-                        print(str(e))
-                    j = j + 1
-                    time.sleep(2)
-
-        except Exception as e:
-            print(str(e))
-
-    return 'cookout'
+    return 'ok'
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='10.1.60.3', port=80, debug=True)
